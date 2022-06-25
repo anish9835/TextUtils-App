60d0583b770ad39629ade630d193e7ea4c111125
anish9835 <86737322+anish9835@users.noreply.github.com>
2022-06-25 22:23:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,10 +46,8 @@ function App() {
       <Router>
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} showAlert={showAlert} />
         <Alert alert={alert} />
-        <TextForm heading="Try TextUtils - word counter, character, remove extra spaces" mode={mode} showAlert={showAlert} />
         <div className="container">
           <Routes>
-            <Route exact path="/TextUtils-App/" element={<TextForm heading="Try TextUtils - word counter, character, remove extra spaces" mode={mode} showAlert={showAlert} />} />
             <Route exact path="/" element={<TextForm heading="Try TextUtils - word counter, character, remove extra spaces" mode={mode} showAlert={showAlert} />} />
             <Route exact path="/about" element={<About mode={mode} toggleMode={toggleMode} />} />
           </Routes>
